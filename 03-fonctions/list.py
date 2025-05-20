aec4dd9167ddc7ecd087d6e248527393ef268c9a
Matthieu Mota <matthieumota@gmail.com>
2025-05-20 14:21:43 +0200

    dict

--- 03-fonctions/list.py
+++ 03-fonctions/list.py
@@ -16,3 +16,51 @@ fruits.pop(1) # Supprimer cerise
 fruits.append('tomate')
 fruits.insert(1, 'orange')
 print(fruits)
+
+your_tuple = (1,)
+print(your_tuple)
+print(your_tuple[0])
+a, b = (1, 2)
+print(a, b)
+print(tuple(sorted((3, 2, 1))))
+
+merged = [1, 2, 3] + [4, 5, 6] + list(your_tuple)
+print(merged)
+
+# Dictionnaires
+person = {
+    'name': 'Mota',
+    'firstname': 'Fiorella',
+    'age': 5,
+    'cats': [
+        { 'name': 'Mina' },
+        { 'name': 'Bianca' },
+    ],
+    'skills': ['Dessiner', 'Jouer'],
+    'is_healthy': False,
+    True: False,
+    0: 18,
+    None: None,
+}
+
+# print(person['firstname'] + ' ' + person['name'])
+
+def full_name(person: dict):
+    return person.get('firstname', '') + ' ' + person.get('name')
+
+print(full_name(person))
+
+print(person.get('cats')[0]['name'])
+
+for cat in person.get('cats'):
+    print(cat.get('name'))
+
+if person.get('is_healthy') == True:
+    print('Tout va bien')
+else:
+    print(f'{full_name(person)} est malade... 🤮')
+
+# Ne pas reproduire à la maison
+print(person.get(0))
+print(person.get(True))
+print(person.get(None))
