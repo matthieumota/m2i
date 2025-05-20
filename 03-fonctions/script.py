dd3649398e9de40f2362977e4f3f7a57dd0000e5
Matthieu Mota <matthieumota@gmail.com>
2025-05-20 12:05:57 +0200

    functions

--- 03-fonctions/script.py
+++ 03-fonctions/script.py
@@ -11,3 +11,12 @@ def say_hello(name: str = None) -> str:
 result = say_hello('Fiorella') + ', ' + say_hello('John')
 say_hello()
 print(result.upper())
+
+def addition(*numbers: int) -> int:
+    return sum(numbers)
+
+print(addition(1, 2) + addition(3, 4))
+print(addition(1, 2, 3, 4, 5, 6, 7, 8, 9))
+
+print(1, 2, 3, 4, 5, sep='-')
+say_hello(name='Andri')
