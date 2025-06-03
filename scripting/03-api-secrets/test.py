8571b78a0e4bea4a72e3a1961ee01ba85bb05219
Matthieu Mota <matthieumota@gmail.com>
2025-06-03 14:12:43 +0200

    prepare dotenv

--- scripting/03-api-secrets/test.py
+++ scripting/03-api-secrets/test.py
@@ -2,11 +2,6 @@ import requests
 
 response = requests.get('https://api.open-meteo.com/v1/forecast?latitude=50.63&longitude=3.06&hourly=temperature_2m&forecast_days=1')
 
-# Faire un appel sur cette URL
-# Récupérer la température dans current
-# Et l'afficher
-'https://api.open-meteo.com/v1/forecast?latitude=50.633&longitude=3.0586&current=temperature_2m&forecast_minutely_15=96&past_minutely_15=96'
-
 if response.status_code == 200:
     json = response.json()
 
@@ -18,4 +13,18 @@ if response.status_code == 200:
     print(json)
 else:
     print(response.headers)
+    print(f'Erreur : {response.status_code}')
+
+# Faire un appel sur cette URL
+# Récupérer la température dans current
+# Et l'afficher
+response = requests.get('https://api.open-meteo.com/v1/forecast?latitude=50.63&longitude=3.06&current=temperature_2m&forecast_minutely_15=96&past_minutely_15=96')
+
+if response.status_code == 200:
+    json = response.json()
+
+    temperature = json['current']['temperature_2m']
+    time = json['current']['time']
+    print(f'La température actuelle est de {temperature} °C et il est {time}')
+else:
     print(f'Erreur : {response.status_code}')
